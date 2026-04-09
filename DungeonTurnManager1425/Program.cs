e5cc434014871c3acaa4801d665e90fbf51ab745
holden.anderson <holden.anderson@students.snow.edu>
2026-04-09 16:24:39 -0600

    made heroes queue up and dequeue for 10 turns, added note for my Final Project (unrelated)

--- DungeonTurnManager1425/Program.cs
+++ DungeonTurnManager1425/Program.cs
@@ -11,21 +11,31 @@ public class Program //BuildYourOwnQueueO51
         Hero Gonzo = new Hero("Gonzo", "Archer", 150); //new one
 
 		Queue<Hero> turnQueue = new Queue<Hero>();
-        //change this to Queue<Hero> not string
-
         
+
+        for (int i = 0; i <10; i++)
+        { 
+            //enqueue just as much as dequeue
         turnQueue.Enqueue(Rorin);
         turnQueue.Enqueue(Aria);
         turnQueue.Enqueue(Gonzo);
+
+        Console.WriteLine($"{turnQueue.Dequeue().Name} topples a giant! 50 damage!\n");//same command prints 
+        Console.WriteLine($"{turnQueue.Dequeue().Name} fires multi-shot explosion spell! 30 damage!\n");// out 3 different 
+        Console.WriteLine($"{turnQueue.Dequeue().Name} must keep up with Aria. Rains down" // things
+        + " volley of arrows!\n");//             ^
+                        //Dequeue print out the (^name property) of whatever Hero sits in Dequeue()
+        }//end loop
+        Console.WriteLine("All turns completed");
 
     }
 
     public class Hero
     {
-        string Name {get; set;} = "RorinDefault"; //must already have a value
+        public string Name {get; set;} = "RorinDefault"; //must already have a value
                                 //but we set it again in each Hero
-        string SoldierClass {get; set;} = "WarriorDefault";
-        int Health {get; set;} //should it be int? or 
+        public string SoldierClass {get; set;} = "WarriorDefault";
+        public int Health {get; set;} //should it be int? or 
     //should i convert like int.Parse, math, and save toString? everytime
         public Hero()
         {
@@ -41,3 +51,25 @@ public class Program //BuildYourOwnQueueO51
         }
     }
 }
+
+//If trying to make an inventory system each class would inherit 
+//Accessory: Iitem
+//Head: Iitem
+//Potion: Iitem
+
+//List<Iitem>
+
+// you would never have just an armor though, like Helmet comes from Armor through abstract
+
+
+//this could be pretty simple to just abstract washer or dryer for my laundry app
+
+// a laundry machine object should have MachineType (washer or dryer)
+//                                      MachineNumber (which machine out of 8)
+//                                      MachineInitialTime
+//                                      MachineTimeLeft
+//                                      Machinebool isFinished
+
+//                                      Machinebool hasNotifiedOwner
+//                                      Machinebool hasNudgeMessagedOwner
+
