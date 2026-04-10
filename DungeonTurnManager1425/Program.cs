ad3789a933a326e28dcbc0941dd9522d107a1871
holden.anderson <holden.anderson@students.snow.edu>
2026-04-09 19:54:20 -0600

    code works well right now. every class is toppling giants tho. next intent: Change returns to continue, Change for loop to while loop

--- DungeonTurnManager1425/Program.cs
+++ DungeonTurnManager1425/Program.cs
@@ -1,30 +1,113 @@
 ﻿using System;
 using System.Dynamic;
+using System.IO;
 
-public class Program //BuildYourOwnQueueO51
+public class Program
 {
 	public static void Main()
 	{
 
         Hero Rorin = new Hero(); //default hero needs no updates
-        Hero Aria = new Hero("Aria", "Mage", 100); //new one update name, class, and health
-        Hero Gonzo = new Hero("Gonzo", "Archer", 150); //new one
+        Hero Aria = new Hero("Aria the Mage", "Mage", 100); //new one update name, class, and health
+        Hero Gonzo = new Hero("Gonzo the Archer", "Archer", 150); //new one
 
-		Queue<Hero> turnQueue = new Queue<Hero>();
+		Queue<Hero> HeroQueue = new Queue<Hero>();
+        //use to take hero turns by dequeue-ing them
+
+        Stack<string> actionHistory = new Stack<string>();
+        //use to push messages onto the history, and pop to undo
+
+        //the game loop
+        for (int i = 0; i <10; i++) //run 10 times
+        { 
+            //enqueue just as much as dequeue in the 10-time-loop
+        HeroQueue.Enqueue(Rorin); 
+        HeroQueue.Enqueue(Aria);
+        HeroQueue.Enqueue(Gonzo);
+
+        Console.WriteLine($"{HeroQueue.Dequeue().Name} topples a giant! 50 damage!\n");//same command prints 
+        actionHistory.Push("Rorin's toppling turn, 50 damage");
+        Console.WriteLine("Would you like to undo turn? Type 'undo', or to continue press Enter");
+        string userInput = Console.ReadLine(); //probably turn this whole if block into
+                                               // another class or even namespaced cs file
+                                               // starting with writeline and readline
+        //userInput = userInput.ToLower().Trim();
+        if (string.IsNullOrWhiteSpace(userInput) || userInput == "enter")
+            {
+                //continue on with the game
+                return;
+            }
+            //continue means we only sanitize the word "undo" if NOT NULL is already confirmed
+        userInput.ToLower().Trim();
+        if (userInput == "undo")
+            {
+                Console.WriteLine("Turn undone: Rorin's toppling turn, 50 damage");
+                actionHistory.Pop();
+            }
+        else
+            {
+                Console.WriteLine("Selection not understood. THE WAR RAGES ON!");
+            }
+
+        //Aria's turn (start of code upper bound limit) Aria fires multi-shot
+        Console.WriteLine($"{HeroQueue.Dequeue().Name} fires multi-shot explosion spell! 30 damage!\n");// out 3 different 
+        //push the action onto the stack
+        actionHistory.Push("Aria's fiery turn, 30 damage");
+        Console.WriteLine("Would you like to undo turn? Type 'undo', or to continue press Enter");
+        userInput = Console.ReadLine(); //probably turn this whole if block into
+                                               // another class or even namespaced cs file
+                                               // starting with writeline and readline
+        //userInput = userInput.ToLower().Trim();
+        if (string.IsNullOrWhiteSpace(userInput) || userInput == "enter")
+            {
+                //continue on with the game
+                return;
+            }
+            //continue means we only sanitize the word "undo" if NOT NULL is already confirmed
+        userInput.ToLower().Trim();
+        if (userInput == "undo")
+            {
+                Console.WriteLine("Turn undone: Arias fiery turn, 30 damage");
+                actionHistory.Pop();
+            }
+        else
+            {
+                Console.WriteLine("Selection not understood. THE WAR RAGES ON!");
+            }
+
+        // lower bound Aria
+
+        //Gonzo's turn (start of code upper bound limit) Gonzo must keep up
+        Console.WriteLine($"{HeroQueue.Dequeue().Name} must keep up with Aria. Rains down" // things
+        + " volley of arrows! 25 damage!\n");//             ^
+                        //Dequeue print out the (^name property) of whatever Hero sits in Dequeue()
+        actionHistory.Push("Gonzo's Bullseye turn, 25 damage");
+        Console.WriteLine("Would you like to undo turn? Type 'undo', or to continue press Enter");
+        userInput = Console.ReadLine(); //probably turn this whole if block into
+                                               // another class or even namespaced cs file
+                                               // starting with writeline and readline
+        //userInput = userInput.ToLower().Trim();
+        if (string.IsNullOrWhiteSpace(userInput) || userInput == "enter")
+            {
+                //continue on with the game
+                return;
+            }
+            //continue means we only sanitize the word "undo" if NOT NULL is already confirmed
+        userInput.ToLower().Trim();
+        if (userInput == "undo")
+            {
+                
+                Console.WriteLine("Turn undone: No more arrows, 30 damage");
+                actionHistory.Pop();
+            }
+        else
+            {
+                Console.WriteLine("Selection not understood. THE WAR RAGES ON!");
+            }
+
+        // lower bound Gonzo
         
 
-        for (int i = 0; i <10; i++)
-        { 
-            //enqueue just as much as dequeue
-        turnQueue.Enqueue(Rorin);
-        turnQueue.Enqueue(Aria);
-        turnQueue.Enqueue(Gonzo);
-
-        Console.WriteLine($"{turnQueue.Dequeue().Name} topples a giant! 50 damage!\n");//same command prints 
-        Console.WriteLine($"{turnQueue.Dequeue().Name} fires multi-shot explosion spell! 30 damage!\n");// out 3 different 
-        Console.WriteLine($"{turnQueue.Dequeue().Name} must keep up with Aria. Rains down" // things
-        + " volley of arrows!\n");//             ^
-                        //Dequeue print out the (^name property) of whatever Hero sits in Dequeue()
         }//end loop
         Console.WriteLine("All turns completed");
 
@@ -50,8 +133,33 @@ public class Program //BuildYourOwnQueueO51
             this.Health = Health;
         }
     }
-}
 
+}
+    /* public class Turnselector()
+    {   
+
+        public static Stack<string> TurnUndo {get; set;}
+        
+        string userInput = Console.ReadLine();
+
+                                userInput = userInput.ToLower().Trim();
+  
+  starting      if (string.IsNullOrWhiteSpace(userInput) || userInput == "enter")
+            {
+                //continue on with the game
+                continue;
+            }
+
+        if (userInput == "undo")
+            {
+                actionHistory.Pop();
+            }Console.WriteLine("Selection not understood. THE WAR RAGES ON!");
+            
+
+
+
+
+//random final project notes, how i might rework what i heard for my thing 
 //If trying to make an inventory system each class would inherit 
 //Accessory: Iitem
 //Head: Iitem
@@ -73,3 +181,4 @@ public class Program //BuildYourOwnQueueO51
 //                                      Machinebool hasNotifiedOwner
 //                                      Machinebool hasNudgeMessagedOwner
 
+*/
